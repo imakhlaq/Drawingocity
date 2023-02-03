7b10f62bcaeca8fe2c24086f5f9dd4df130ab190
imakhlaq <imakhlaqXD@gmail.com>
2023-02-03 20:01:36 +0530

    custom hook

--- hooks/useDraw.ts
+++ hooks/useDraw.ts
@@ -4,7 +4,22 @@ export const useDraw = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   useEffect(() => {
     //handler
-    const handler = (event: MouseEvent) => {};
+    const handler = (event: MouseEvent) => {
+      const currentValue = computePointsInCanvas(event);
+    };
+
+    //computing poits
+    const computePointsInCanvas = (event: MouseEvent) => {
+      const canvas = canvasRef.current;
+
+      if (!canvas) return;
+
+      const rect = canvas.getBoundingClientRect();
+      const y = event.clientX - rect.left;
+      const x = event.clientY - rect.top;
+
+      return { x, y };
+    };
 
     //add event listner
     canvasRef.current?.addEventListener("mousemove", handler);
