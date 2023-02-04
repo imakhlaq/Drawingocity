ff33fc67b1151da3b9d29ef77b19c0def3c25bf8
imakhlaq <imakhlaqXD@gmail.com>
2023-02-04 20:27:23 +0530

    UI

--- app/header.tsx
+++ app/header.tsx
@@ -10,24 +10,16 @@ type Props = {
 const Header = ({ setColor, color, clear }: Props) => {
   const [showPallet, setShowPallet] = useState(false);
 
-  useEffect(() => {
-    if (showPallet) {
-      window.addEventListener("click", () => setShowPallet(false));
-    }
-    return () =>
-      window.removeEventListener("click", () => setShowPallet(false));
-  }, []);
-
   return (
-    <div className="bg-[#BDCDD6] px-3 py-4 flex gap-10 justify-between">
+    <div className="bg-[#20262E] px-3 py-4 flex gap-10 justify-between select-none">
       <div>
-        <h1 className="text-3xl font-semibold">Canvascity</h1>
+        <h1 className="text-3xl font-semibold text-white">Canvascity</h1>
       </div>
       <div className="flex justify-center items-center gap-10">
         <div
           onClick={() => setShowPallet((show) => !show)}
           style={{ background: color }}
-          className={`w-8 h-8 rounded-full cursor-pointer relative shadow-xl`}
+          className={`w-8 h-8 rounded-full cursor-pointer relative shadow-xl border`}
         ></div>
         {showPallet && (
           <div className="absolute top-24 right-20">
@@ -37,7 +29,7 @@ const Header = ({ setColor, color, clear }: Props) => {
         <button
           type="button"
           onClick={clear}
-          className="px-10 py-2 rounded-lg border bg-white shadow-xl text-lg font-semibold"
+          className="px-10 py-2 rounded-lg border bg-white shadow-xl text-lg font-semibold select-none"
         >
           Clear
         </button>
